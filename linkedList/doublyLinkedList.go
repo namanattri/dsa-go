0d724423f4726328859cc703b599005c78ef1310
Naman Attri <namanattri5@gmail.com>
2023-05-06 17:06:33 +0530

    Implements doubly linked list along with all ops

---
+++ linkedList/doublyLinkedList.go
@@ -0,0 +1,122 @@
+package linkedList
+
+import "fmt"
+
+type DoublyLinkedListNode struct {
+	Value int
+	Prev  *DoublyLinkedListNode
+	Next  *DoublyLinkedListNode
+}
+
+func NewDoublyLinkeListNode(value int) *DoublyLinkedListNode {
+	return &DoublyLinkedListNode{Value: value}
+}
+
+type DoublyLinkedList struct {
+	head *DoublyLinkedListNode
+	tail *DoublyLinkedListNode
+}
+
+func NewDoublyLinkeList(values []int) *DoublyLinkedList {
+	head := (*DoublyLinkedListNode)(nil)
+	tail := (*DoublyLinkedListNode)(nil)
+	for _, value := range values {
+		node := NewDoublyLinkeListNode(value)
+		if head == nil {
+			head = node
+			tail = node
+			continue
+		}
+
+		node.Prev = tail
+		tail.Next = node
+		tail = node
+	}
+
+	return &DoublyLinkedList{head: head, tail: tail}
+}
+
+func (l *DoublyLinkedList) Traverse() {
+	cursor := l.head
+	for cursor != nil {
+		fmt.Printf("%d -> ", cursor.Value)
+		cursor = cursor.Next
+	}
+	fmt.Println("X")
+}
+
+func (l *DoublyLinkedList) TraverseBackwards() {
+	cursor := l.tail
+	for cursor != nil {
+		fmt.Printf("%d -> ", cursor.Value)
+		cursor = cursor.Prev
+	}
+	fmt.Println("X")
+}
+
+func (l *DoublyLinkedList) InsertStart(value int) {
+	node := NewDoublyLinkeListNode(value)
+	node.Next = l.head
+	l.head.Prev = node
+	l.head = node
+}
+
+func (l *DoublyLinkedList) InsertEnd(value int) {
+	node := NewDoublyLinkeListNode(value)
+	node.Prev = l.tail
+	l.tail.Next = node
+	l.tail = node
+}
+
+func (l *DoublyLinkedList) InsertPos(value int, pos int) {
+	if pos == 0 {
+		l.InsertStart(value)
+		return
+	}
+
+	cursor := l.head
+	nodeIndex := 0
+
+	for cursor != nil {
+		if nodeIndex == pos {
+			node := NewDoublyLinkeListNode(value)
+			cursor.Prev.Next = node
+			node.Prev = cursor.Prev
+			cursor.Prev = node
+			node.Next = cursor
+			return
+		}
+		cursor = cursor.Next
+		nodeIndex++
+	}
+}
+
+func (l *DoublyLinkedList) DeleteStart() {
+	l.head.Next.Prev = nil
+	l.head = l.head.Next
+}
+
+func (l *DoublyLinkedList) DeleteEnd() {
+	l.tail.Prev.Next = nil
+	l.tail = l.tail.Prev
+}
+
+func (l *DoublyLinkedList) DeletePos(pos int) {
+	if pos == 0 {
+		l.DeleteStart()
+		return
+	}
+
+	cursor := l.head
+	nodeIndex := 0
+
+	for cursor != nil {
+		if nodeIndex == pos {
+			cursor.Prev.Next = cursor.Next
+			cursor.Next.Prev = cursor.Prev
+			return
+		}
+		cursor = cursor.Next
+		nodeIndex++
+	}
+}
---
+++ ops/doublyLinkedListOps.go
@@ -0,0 +1,44 @@
+package ops
+
+import (
+	"dsa-go/linkedList"
+	"fmt"
+)
+
+func DoublyLinkedListOps() {
+	l := linkedList.NewDoublyLinkeList([]int{7, 22, 13, 34, 76, 2, 88})
+	fmt.Println("Traversing the singly linked list: ")
+	l.Traverse()
+	fmt.Println("Traversing the singly linked list backwards: ")
+	l.TraverseBackwards()
+
+	fmt.Printf("Inserting %d at the begining of the linked list\n", 4)
+	l.InsertStart(4)
+	l.Traverse()
+	l.TraverseBackwards()
+
+	fmt.Printf("Inserting %d at the end of the linked list\n", 8)
+	l.InsertEnd(8)
+	l.Traverse()
+	l.TraverseBackwards()
+
+	fmt.Printf("Inserting %d at index %d of the linked list\n", 15, 3)
+	l.InsertPos(15, 3)
+	l.Traverse()
+	l.TraverseBackwards()
+
+	fmt.Println("Deleting a node from the begining of the linked list")
+	l.DeleteStart()
+	l.Traverse()
+	l.TraverseBackwards()
+
+	fmt.Println("Deleting a node from the tail of the linked list")
+	l.DeleteEnd()
+	l.Traverse()
+	l.TraverseBackwards()
+
+	fmt.Printf("Deleting a node at index %d of the linked list\n", 4)
+	l.DeletePos(4)
+	l.Traverse()
+	l.TraverseBackwards()
+}
