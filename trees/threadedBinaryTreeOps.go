6019ab2aa4461a4d6cf343c2c57419ca6e6d6e3d
Naman Attri <namanattri5@gmail.com>
2023-06-04 22:22:43 +0530

    Adds ops for testing right insertion into threaded binary tree

--- trees/threadedBinaryTreeOps.go
+++ trees/threadedBinaryTreeOps.go
@@ -7,15 +7,38 @@ func ThreadedBinaryTreeOps() {
 
 	t.CreateInorderThreaded()
 
+	fmt.Print("Inorder traversal: ")
 	t.InOrderTraversalInOrderTree()
 	fmt.Println()
 
+	fmt.Print("Inorder traversal(alt): ")
 	t.InOrderTraversalInOrderTreeAlt()
 	fmt.Println()
 
+	fmt.Print("Preorder traversal: ")
 	t.PreOrderTraversalInOrderTree()
 	fmt.Println()
 
+	fmt.Print("Preorder traversal(alt): ")
+	t.PreOrderTraversalInOrderTreeAlt()
+	fmt.Println()
+
+	t.InsertRightOfInOrderTree(t.root.left, 7)
+	fmt.Printf("Inserting %d to the right of %d\n", 7, t.root.left.value)
+
+	fmt.Print("Inorder traversal: ")
+	t.InOrderTraversalInOrderTree()
+	fmt.Println()
+
+	fmt.Print("Inorder traversal(alt): ")
+	t.InOrderTraversalInOrderTreeAlt()
+	fmt.Println()
+
+	fmt.Print("Preorder traversal: ")
+	t.PreOrderTraversalInOrderTree()
+	fmt.Println()
+
+	fmt.Print("Preorder traversal(alt): ")
 	t.PreOrderTraversalInOrderTreeAlt()
 	fmt.Println()
 }
