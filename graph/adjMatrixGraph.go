7b9cfca4d017b6c0257beb5f52f49f8f8f3188de
Naman Attri <namanattri5@gmail.com>
2023-06-10 14:14:54 +0530

    Implements a method to create an edge in a graph

--- graph/adjMatrixGraph.go
+++ graph/adjMatrixGraph.go
@@ -7,15 +7,19 @@ type AdjMatrixGraph struct {
 	E            int
 	adj          [][]int
 	vertexLabels []rune
+	directed     bool
 }
 
-func NewAdjMatrixGraph(V int, E int, vertexLabels []rune) *AdjMatrixGraph {
-	g := &AdjMatrixGraph{V: V, E: E, vertexLabels: vertexLabels}
+func NewAdjMatrixGraph(V int, E int, vertexLabels []rune, directed bool) *AdjMatrixGraph {
+	g := &AdjMatrixGraph{V: V, E: E, vertexLabels: vertexLabels, directed: directed}
 	g.adj = make([][]int, V)
 	for u := 0; u < V; u++ {
 		g.adj[u] = make([]int, V)
 		for v := 0; v < V; v++ {
 			g.adj[u][v] = 0
+			if u == v && !directed {
+				g.adj[u][v] = 1
+			}
 		}
 	}
 	return g
@@ -36,4 +40,13 @@ func (g *AdjMatrixGraph) String() string {
 		res += row + "|\n"
 	}
 	return res
+}
+
+func (g *AdjMatrixGraph) CreateEdge(u int, v int) {
+	fmt.Printf("Creating edge (%d, %d)\n", u, v)
+	g.adj[u][v] = 1
+
+	if !g.directed {
+		g.adj[v][u] = 1
+	}
 }
--- graph/adjMatrixGraphOps.go
+++ graph/adjMatrixGraphOps.go
@@ -3,7 +3,21 @@ package graph
 import "fmt"
 
 func AdjMatrixGraphOps() {
-	g := NewAdjMatrixGraph(4, 5, []rune{'A', 'B', 'C', 'D'})
+	g1 := NewAdjMatrixGraph(4, 5, []rune{'A', 'B', 'C', 'D'}, true)
 
-	fmt.Println(g)
+	fmt.Println("Empty undirected graph")
+	fmt.Println(g1)
+
+	g1.CreateEdge(0, 1)
+	fmt.Println(g1)
+	g1.CreateEdge(0, 3)
+	fmt.Println(g1)
+	g1.CreateEdge(1, 2)
+	fmt.Println(g1)
+	g1.CreateEdge(2, 0)
+	fmt.Println(g1)
+	g1.CreateEdge(2, 3)
+	fmt.Println(g1)
+
+	fmt.Println(g1)
 }
