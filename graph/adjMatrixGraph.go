bdb935ba10dc6a35085b197be1e65e34b7cfb42a
Naman Attri <namanattri5@gmail.com>
2023-06-11 09:40:41 +0530

    refactors code for adj matrix graph

--- graph/adjMatrixGraph.go
+++ graph/adjMatrixGraph.go
@@ -3,21 +3,27 @@ package graph
 import "fmt"
 
 type AdjMatrixGraph struct {
-	V            int
-	E            int
 	adj          [][]int
-	vertexLabels []rune
+	vertices     []rune
 	directed     bool
 	visited      []bool
+	labelToIndex map[rune]int
 }
 
-func NewAdjMatrixGraph(V int, E int, vertexLabels []rune, directed bool) *AdjMatrixGraph {
-	g := &AdjMatrixGraph{V: V, E: E, vertexLabels: vertexLabels, directed: directed}
-	g.adj = make([][]int, V)
-	g.visited = make([]bool, V)
-	for u := 0; u < V; u++ {
-		g.adj[u] = make([]int, V)
-		for v := 0; v < V; v++ {
+func NewAdjMatrixGraph(vertices []rune, directed bool) *AdjMatrixGraph {
+	g := &AdjMatrixGraph{vertices: vertices, directed: directed}
+
+	countOfVertices := len(vertices)
+
+	g.adj = make([][]int, countOfVertices)
+	g.visited = make([]bool, countOfVertices)
+	g.labelToIndex = make(map[rune]int)
+
+	for u := 0; u < countOfVertices; u++ {
+		g.adj[u] = make([]int, countOfVertices)
+		g.labelToIndex[g.vertices[u]] = u
+
+		for v := 0; v < countOfVertices; v++ {
 			g.adj[u][v] = 0
 			if u == v && !directed {
 				g.adj[u][v] = 1
@@ -29,14 +35,16 @@ func NewAdjMatrixGraph(V int, E int, vertexLabels []rune, directed bool) *AdjMat
 
 func (g *AdjMatrixGraph) String() string {
 	res := " | "
-	for u := 0; u < g.V; u++ {
-		res += fmt.Sprintf("%c ", g.vertexLabels[u])
+	countOfVertices := len(g.vertices)
+
+	for u := 0; u < countOfVertices; u++ {
+		res += fmt.Sprintf("%c ", g.vertices[u])
 	}
 	res += "|\n"
 
-	for u := 0; u < g.V; u++ {
-		row := fmt.Sprintf("%c| ", g.vertexLabels[u])
-		for v := 0; v < g.V; v++ {
+	for u := 0; u < countOfVertices; u++ {
+		row := fmt.Sprintf("%c| ", g.vertices[u])
+		for v := 0; v < countOfVertices; v++ {
 			row += fmt.Sprintf("%d ", g.adj[u][v])
 		}
 		res += row + "|\n"
@@ -44,17 +52,18 @@ func (g *AdjMatrixGraph) String() string {
 	return res
 }
 
-func (g *AdjMatrixGraph) CreateEdge(u int, v int) {
-	fmt.Printf("Creating edge (%d, %d)\n", u, v)
-	g.adj[u][v] = 1
+func (g *AdjMatrixGraph) CreateEdge(u rune, v rune) {
+	fmt.Printf("Creating edge (%c, %c)\n", u, v)
+	g.adj[g.labelToIndex[u]][g.labelToIndex[v]] = 1
 
 	if !g.directed {
-		g.adj[v][u] = 1
+		g.adj[g.labelToIndex[v]][g.labelToIndex[u]] = 1
 	}
 }
 
 func (g *AdjMatrixGraph) DFSTraversal() {
-	for i := 0; i < g.V; i++ {
+	countOfVertices := len(g.vertices)
+	for i := 0; i < countOfVertices; i++ {
 		if !g.visited[i] {
 			g.DFS(i)
 		}
@@ -62,10 +71,11 @@ func (g *AdjMatrixGraph) DFSTraversal() {
 }
 
 func (g *AdjMatrixGraph) DFS(u int) {
-	fmt.Printf("%c ", g.vertexLabels[u])
+	fmt.Printf("%c ", g.vertices[u])
 	g.visited[u] = true
+	countOfVertices := len(g.vertices)
 
-	for v := 0; v < g.V; v++ {
+	for v := 0; v < countOfVertices; v++ {
 		if !g.visited[v] && g.adj[u][v] == 1 {
 			g.DFS(v)
 		}
--- graph/adjMatrixGraphOps.go
+++ graph/adjMatrixGraphOps.go
@@ -3,22 +3,26 @@ package graph
 import "fmt"
 
 func AdjMatrixGraphOps() {
-	g := NewAdjMatrixGraph(4, 5, []rune{'A', 'B', 'C', 'D'}, true)
+	g := NewAdjMatrixGraph([]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}, false)
 
 	fmt.Println("Empty undirected graph")
 	fmt.Println(g)
 
-	g.CreateEdge(0, 1)
+	g.CreateEdge('A', 'B')
 	fmt.Println(g)
-	g.CreateEdge(0, 3)
+	g.CreateEdge('B', 'C')
 	fmt.Println(g)
-	g.CreateEdge(1, 2)
+	g.CreateEdge('B', 'H')
 	fmt.Println(g)
-	g.CreateEdge(2, 0)
+	g.CreateEdge('C', 'D')
 	fmt.Println(g)
-	g.CreateEdge(2, 3)
+	g.CreateEdge('C', 'E')
 	fmt.Println(g)
-
+	g.CreateEdge('E', 'H')
+	fmt.Println(g)
+	g.CreateEdge('E', 'F')
+	fmt.Println(g)
+	g.CreateEdge('E', 'G')
 	fmt.Println(g)
 
 	g.DFSTraversal()
